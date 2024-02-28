3fd84042e118330672277b96c423b67f61beb092
Nurzatbek Dunganov <75665162+Nurzatbek@users.noreply.github.com>
2024-02-28 08:03:52 +0300

    Add files via upload
    test-railway

---
+++ test-railway/test-railway/src/main/java/com/example/testrailway/TestRailwayApplication.java
@@ -0,0 +1,13 @@
+package com.example.testrailway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRailwayApplication.class, args);
+	}
+
+}
